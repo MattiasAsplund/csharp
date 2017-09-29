b3540718089e592f5cafbc9f0e43e09a5892d865
Mattias Asplund <mattias@mattiasasplund.se>
2017-09-29 13:21:54 +0200

    ByggahusApp

--- ByggahusApp/Program.cs
+++ ByggahusApp/Program.cs
@@ -13,15 +13,22 @@ namespace ByggahusApp
             House aHouse = new House();
             Window window1 = new Window();
             Window window2 = new Window();
+            Window window3 = new Window();
+            Window window4 = new Window();
             Room room1 = new Room();
             Room room2 = new Room();
+            Room room3 = new Room();
+            Room room4 = new Room();
             room1.AddWindow(window1);
             room2.AddWindow(window2);
+            room2.AddWindow(window3);
+            room2.AddWindow(window4);
             aHouse.AddRoom(room1);
             aHouse.AddRoom(room2);
+            aHouse.AddRoom(room3);
+            aHouse.AddRoom(room4);
             window1.Open();
-            int numberOfClosedWindows = aHouse.NumberOfClosedWindows;
-            Console.WriteLine();
+            Console.WriteLine(aHouse.NumberOfClosedWindows);
         }
     }
 }
