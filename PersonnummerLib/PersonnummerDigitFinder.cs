80056ce37ca0b9b6eefef1fc71d11edccb0bc244
Mattias Asplund <mattias@mattiasasplund.se>
2017-10-04 10:36:28 +0200

    Naiv implementation av PersonnummerDigitFinder

---
+++ PersonnummerLib/PersonnummerDigitFinder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonnummerLib
+{
+    public class PersonnummerDigitFinder
+    {
+        public string LastDigit {
+            get
+            {
+                return "1";
+            }
+        }
+
+        public void Process(string firstNine)
+        {
+            
+        }
+    }
+}
---
+++ PersonnummerLibTests/PersonnummerDigitFinderTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PersonnummerLib;
+
+namespace PersonnummerLibTests
+{
+    [TestClass]
+    public class PersonnummerDigitFinderTests
+    {
+        [TestMethod]
+        public void Mattias()
+        {
+            var sut = new PersonnummerDigitFinder();
+            sut.Process("710405785");
+            var expected = "1";
+            Assert.AreEqual(expected, sut.LastDigit);
+        }
+        [TestMethod]
+        public void Jazmina()
+        {
+            var sut = new PersonnummerDigitFinder();
+            sut.Process("711207922");
+            var expected = "8";
+            Assert.AreEqual(expected, sut.LastDigit);
+        }
+    }
+}
