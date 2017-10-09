fdde944247acebd58d4979234dc1342a912a9806
Mattias Asplund <mattias@mattiasasplund.se>
2017-10-09 09:30:21 +0200

    Regex övning

---
+++ RegexTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RegexTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RegexTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9b7f11e3-0d86-4573-9ad2-1dceaa25db9a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RegexTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+namespace RegexTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Konto()
+        {
+            string pattern = @"#KONTO (\d{4}) \""?([^\""]*)\""?";
+            string input = @"#KONTO 1010 Patent";
+
+            Match m = Regex.Match(input, pattern);
+            Assert.AreEqual("1010", m.Groups[1].Value);
+            Assert.AreEqual("Patent", m.Groups[2].Value);
+        }
+        [TestMethod]
+        public void Verifikat()
+        {
+            string pattern = @"#VER . -?. (\d{8}) \""?([^\""]*)\""? (\d{8})";
+            string input = @"#VER A 1 20150902 ""Utbetalningsjournal nr 136"" 20160122";
+            Match m = Regex.Match(input, pattern);
+            Assert.AreEqual("20150902", m.Groups[1].Value);
+            Assert.AreEqual("Utbetalningsjournal nr 136", m.Groups[2].Value);
+            Assert.AreEqual("20160122", m.Groups[3].Value);
+        }
+    }
+}
