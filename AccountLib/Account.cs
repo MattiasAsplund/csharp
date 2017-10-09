ce301c222e8e060d44a79db96916117829deb5a0
Mattias Asplund <mattias@mattiasasplund.se>
2017-10-09 13:35:38 +0200

    Account with tests

--- AccountLib/Account.cs
+++ AccountLib/Account.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AccountLib
 {
     public class Account
     {
-
+        public Account(string Code, string Description, decimal amount, bool isSummary)
+        {
+            this.Code = Code;
+            this.Description = Description;
+            this.amount = amount;
+            this.IsSummary = isSummary;
+            Children = new List<Account>();
+        }
+        public string Code;
+        private decimal amount;
+        public string Description { get; set; }
+        public decimal Amount {
+            get
+            {
+                if (IsSummary)
+                    return Children.Sum(child => child.Amount);
+                return amount;
+            }
+        }
+        public bool IsSummary { get; set; }
+        public List<Account> Children { get; set; }
     }
 }
--- AccountTestsLib/AccountTests.cs
+++ AccountTestsLib/AccountTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AccountLib;
 
 namespace AccountTestsLib
 {
@@ -9,7 +10,16 @@ namespace AccountTestsLib
         [TestMethod]
         public void Sum()
         {
-
+            Account assets = new Account("A", "Assets", 0, true);
+            Account cars = new Account("1010", "Car fleet", 0, true);
+            Account volvo = new Account("1030", "Volvo fleet", 50000000, false);
+            Account tesla = new Account("1040", "Tesla fleet", 20000000, false);
+            cars.Children.Add(volvo);
+            cars.Children.Add(tesla);
+            Account spacerockets = new Account("1020", "Rockets", 750000000, false);
+            assets.Children.Add(cars);
+            assets.Children.Add(spacerockets);
+            Assert.AreEqual(assets.Amount, 820000000);
         }
     }
 }
